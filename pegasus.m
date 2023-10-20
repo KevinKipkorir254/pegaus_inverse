3e127d78ef48de6f4ebf9b83602172df12e7be83
KevinKipkorir254 <kipkorir.magut254@gmail.com>
2023-10-20 14:03:19 +0300

    this fix is what will work with the four DOF robot

--- pegasus.m
+++ pegasus.m
@@ -1,20 +1,20 @@
+
 clc
-L1 = Link('d', 0.1, 'a', 0, 'alpha', -pi/2);
-L2 = Link('d', 0, 'a', 0.1, 'alpha', 0);
-L3 = Link('d', 0, 'a', 0.1, 'alpha', 0);
+L1 = Link('d', 0.04, 'a', 0, 'alpha', -pi/2);
+L2 = Link('d', 0, 'a', 0.15, 'alpha', 0);
+L3 = Link('d', 0, 'a', 0.12, 'alpha', 0);
 L4 = Link('d', 0, 'a', 0, 'alpha', -pi/2);
 L5 = Link('d', 0.1, 'a', 0, 'alpha', 0);
-
 dh_params = [L1 L2 L3 L4 L5];
 % Create a robot model using the DH parameters
 robot = SerialLink(dh_params, 'name', 'PEGASUS');
 
-robot.plot([0.000000, -0.643501, 1.287002, -0.643501, 1.57])
+robot.plot([0.674741, -1.285030, 2.059458, 0.795572, 3.14])
 %robot.teach();
 
 
 
-x=robot.fkine([0.000000, -0.643501, 1.287002, -0.643501, 1.57])
+x=robot.fkine([0.674741, -1.285030, 2.059458, 0.795572, 3.14])
 j = x.t(1,1)
 j = x.t(2,1)
 j = x.t(3,1)
