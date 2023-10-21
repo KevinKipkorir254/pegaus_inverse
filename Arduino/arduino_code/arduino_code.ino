029559b4e5ae935ac22a260ca6cdfbc218ffe2a3
KevinKipkorir254 <kipkorir.magut254@gmail.com>
2023-10-21 22:39:56 +0300

    receives command and updates the PWM

--- Arduino/arduino_code/arduino_code.ino
+++ Arduino/arduino_code/arduino_code.ino
@@ -1,5 +1,28 @@
+#include <ArduinoJson.h>
+#include <Servo.h>
+
+
+//declaring space requirements
+StaticJsonDocument<200> doc;
+
+
+int base = 11;
+int shoulder = 12;
+int elbow = 13;
+int wrist = 6;
+
+Servo base_servo;
+Servo shoulder_servo;
+Servo elbow_servo;
+Servo wrist_servo;
+
 void setup() {
   Serial.begin(9600); // Initialize serial communication at 9600 baud
+
+  base_servo.attach(base);
+  shoulder_servo.attach(shoulder);
+  elbow_servo.attach(elbow);
+  wrist_servo.attach(wrist);
 }
 
 void loop() {
@@ -9,8 +32,40 @@ void loop() {
     String receivedData = Serial.readStringUntil('\n'); // Read data until a newline character is encountered
     
     if (receivedData.length() > 0) {
-      receivedData.trim(); 
-      Serial.print(receivedData);
+
+      
+      // Parse the JSON data
+      DeserializationError error = deserializeJson(doc, receivedData);
+      
+      if (error) {
+        Serial.print("JSON parsing error: ");
+        Serial.println(error.c_str());
+      } else {
+        // Access JSON values
+        int joint_1 = doc["joint_1"];
+        int joint_2 = doc["joint_2"];
+        int joint_3 = doc["joint_3"];
+        int joint_4 = doc["joint_4"];
+        
+        // Use the joint values as needed
+        Serial.print("Received JSON data - joint_1: ");
+        Serial.println(joint_1);
+        Serial.print("joint_2: ");
+        Serial.println(joint_2);
+        Serial.print("joint_3: ");
+        Serial.println(joint_3);
+        Serial.print("joint_4: ");
+        Serial.println(joint_4);
+
+
+        
+       base_servo.write(joint_1);
+       shoulder_servo.write(joint_2);
+       elbow_servo.write(joint_3);
+       wrist_servo.write(joint_4);
+      }
+
+      
     }
   }
 }
